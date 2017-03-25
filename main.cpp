620e970942b5760a2983fcf522542c1951043b03
Алина Можина <mohinalina@gmail.com>
2017-03-26 00:48:40 +0700

    Created Visual Studio project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <QCoreApplication>
 #include "task1.h"
 #include "task2.h"
 #include "task3.h"
@@ -6,15 +5,17 @@
 #include "task5.h"
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
     mvms_2017::Task1 x(true);
     mvms_2017::Task2 x1(true);
     mvms_2017::Task3 x2(true);
     mvms_2017::Task4 x3(true);
     mvms_2017::Task5 x4(true);
-    try{
-        x.drawCircle("Test",1,1,1);
-    }catch(std::exception ex){
+    try
+    {
+        x.drawCircle("Test", 1, 1, 1);
+    }
+    catch (std::exception ex)
+    {
         std::cout << ex.what();
     }
 
