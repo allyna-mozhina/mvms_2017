6bc811286ad055be1d4da888b4985381199d56e4
Алина Можина <mohinalina@gmail.com>
2017-04-09 03:13:12 +0700

    Added class declaration - Task 2

---
+++ Mozhina_Alina_201731143_Task2.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "task2.h"
+
+namespace mvms_2017
+{
+    struct Color
+    {
+        float red, green, blue;
+        Color(float red, float green, float blue);
+    };
+
+    class Mozhina_Alina_201731143_Task2 : public Task2
+    {
+    private:
+        const int VARIANT = 201731143;
+        const std::string FIRST_NAME = "Алина";
+        const std::string LAST_NAME = "Можина";
+
+        using Filter = std::vector<std::vector<double>>;
+        Filter computeGauss(int filter_size, float sigma) const;
+
+        void setPixel(cv::Mat &image, float x, float y, Color rgb) const;
+        Color getPixel(cv::Mat &image, float x, float y) const;
+
+        Color convolution(cv::Mat &image, const Filter &filter, float x, float y) const;
+        cv::Mat convolution(cv::Mat &image, const Filter &filter) const;
+ 
+    public:
+        Mozhina_Alina_201731143_Task2(bool verbose);
+
+        int variant() override;
+        std::string getFirstName() override;
+        std::string getSecondName() override;
+
+        cv::Mat gauss(cv::Mat image, int filter_size, float sigma);
+    };
+}
