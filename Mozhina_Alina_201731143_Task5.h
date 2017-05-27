ad2d179d8ee31444f165698121e04c37e05be01f
Алина Можина <mohinalina@gmail.com>
2017-05-27 22:36:33 +0700

    Added class declaration - Task 5

---
+++ Mozhina_Alina_201731143_Task5.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "task5.h"
+
+namespace mvms_2017
+{
+    struct Color
+    {
+        float red, green, blue;
+        Color(float red, float green, float blue);
+    };
+
+    class Mozhina_Alina_201731143_Task5 : public Task5
+    {
+    private:
+        const int VARIANT = 201731143;
+        const std::string FIRST_NAME = "Алина";
+        const std::string LAST_NAME = "Можина";
+
+        void setPixel(cv::Mat &image, int x, int y, Color rgb) const;
+        Color getPixel(const cv::Mat &image, int x, int y) const;
+
+    public:
+        Mozhina_Alina_201731143_Task5(bool verbose);
+
+        int variant() override;
+        std::string getFirstName() override;
+        std::string getSecondName() override;
+        
+        cv::Mat correctLensDistorsions(cv::Mat image, double r2, double r4, double k1, double k2);
+    };
+}
